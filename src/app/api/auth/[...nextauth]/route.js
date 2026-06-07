6b9bbc1c094224ec530670a11a90d06f4bef1e68
Vishal <vishalsoam23@gmail.com>
2026-06-07 15:58:30 +0530

    fix login error

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
-import NextAuth from "next-auth";
+import NextAuthImport from "next-auth";
 import { authOptions } from "@/lib/auth";
 
+const NextAuth = NextAuthImport?.default ?? NextAuthImport;
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
